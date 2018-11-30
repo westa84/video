5d33e8805432e6463d333e5b7b8a568e3cfb14c2
Mikolaj Wesolowski <westa@poczta.onet.pl>
2018-11-30 15:02:17 +0100

    Updates

--- main.js
+++ main.js
@@ -61,7 +61,7 @@ __webpack_require__.r(__webpack_exports__);
 
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.title = 'Mikolaj Wesolowski';
+        this.title = 'Lista filmów';
         this.color = 'pink';
     }
     Object.defineProperty(AppComponent.prototype, "styles", {
@@ -130,6 +130,120 @@ var AppModule = /** @class */ (function () {
 
 /***/ }),
 
+/***/ "./src/app/latest-video.service.ts":
+/*!*****************************************!*\
+  !*** ./src/app/latest-video.service.ts ***!
+  \*****************************************/
+/*! exports provided: LatestVideoService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LatestVideoService", function() { return LatestVideoService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var LatestVideoService = /** @class */ (function () {
+    function LatestVideoService() {
+    }
+    LatestVideoService.prototype.fetchMovies = function () {
+        var movies = [
+            {
+                title: "Lost Highway",
+                year: "1997",
+                description: "Saksofonista zostaje aresztowany pod zarzutem zamordowania swojej żony. Po kilku dniach znika z celi, a na jego miejscu pojawia się inny mężczyzna. ",
+                duration: "135",
+                rate: "7,6"
+            },
+            {
+                title: "Mulholland Drive",
+                year: "2001",
+                description: "Saksofonista zostaje aresztowany pod zarzutem zamordowania swojej żony. Po kilku dniach znika z celi, a na jego miejscu pojawia się inny mężczyzna. Rita traci pamięć wskutek wypadku samochodowego. Odkrywa, że jej torba jest pełna pieniędzy i postanawia dowiedzieć się, skąd one pochodzą.",
+                duration: "146",
+                rate: "7,6"
+            }
+        ];
+        return new Promise(function (resolve) {
+            setTimeout(function () {
+                resolve(movies);
+            }, 500);
+        });
+    };
+    LatestVideoService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+            providedIn: 'root'
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], LatestVideoService);
+    return LatestVideoService;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/video-detail/video-detail.component.css":
+/*!*********************************************************!*\
+  !*** ./src/app/video-detail/video-detail.component.css ***!
+  \*********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3ZpZGVvLWRldGFpbC92aWRlby1kZXRhaWwuY29tcG9uZW50LmNzcyJ9 */"
+
+/***/ }),
+
+/***/ "./src/app/video-detail/video-detail.component.html":
+/*!**********************************************************!*\
+  !*** ./src/app/video-detail/video-detail.component.html ***!
+  \**********************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<p *ngIf=\"!video\">\n  nie ma danych\n</p>\n\n<fieldset *ngIf=\"video\">\n    <legend>Film</legend>\n\n    <h2>\n        {{ video.title }}\n        <small>\n            ( {{ video.year }} )\n        </small>\n    </h2>\n    <p>\n        Czas trwania: {{ video.duration }}\n    </p>\n    <p>{{ video.description }}</p>\n</fieldset>\n"
+
+/***/ }),
+
+/***/ "./src/app/video-detail/video-detail.component.ts":
+/*!********************************************************!*\
+  !*** ./src/app/video-detail/video-detail.component.ts ***!
+  \********************************************************/
+/*! exports provided: VideoDetailComponent */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "VideoDetailComponent", function() { return VideoDetailComponent; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var VideoDetailComponent = /** @class */ (function () {
+    function VideoDetailComponent() {
+        this.video = null;
+    }
+    VideoDetailComponent.prototype.ngOnInit = function () {
+    };
+    tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Input"])(),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:type", Object)
+    ], VideoDetailComponent.prototype, "video", void 0);
+    VideoDetailComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-video-detail',
+            template: __webpack_require__(/*! ./video-detail.component.html */ "./src/app/video-detail/video-detail.component.html"),
+            styles: [__webpack_require__(/*! ./video-detail.component.css */ "./src/app/video-detail/video-detail.component.css")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], VideoDetailComponent);
+    return VideoDetailComponent;
+}());
+
+
+
+/***/ }),
+
 /***/ "./src/app/video-list/video-list.component.css":
 /*!*****************************************************!*\
   !*** ./src/app/video-list/video-list.component.css ***!
@@ -148,7 +262,7 @@ module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-module.exports = "<p>\n  video-list works!\n</p>\n"
+module.exports = "<ul>\n  <li *ngFor=\"let movie of movies\">\n\n    <app-video-detail [video]=\"movie\"></app-video-detail>\n\n  </li>\n</ul>"
 
 /***/ }),
 
@@ -164,20 +278,32 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "VideoListComponent", function() { return VideoListComponent; });
 /* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
 /* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _latest_video_service__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../latest-video.service */ "./src/app/latest-video.service.ts");
+
 
 
 var VideoListComponent = /** @class */ (function () {
-    function VideoListComponent() {
+    function VideoListComponent(latestVideoService) {
+        this.latestVideoService = latestVideoService;
+        this.movies = [];
     }
     VideoListComponent.prototype.ngOnInit = function () {
+        var _this = this;
+        this.latestVideoService.fetchMovies()
+            .then(function (movies) {
+            _this.movies = movies;
+        });
     };
     VideoListComponent = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
             selector: 'app-video-list',
             template: __webpack_require__(/*! ./video-list.component.html */ "./src/app/video-list/video-list.component.html"),
+            providers: [
+                _latest_video_service__WEBPACK_IMPORTED_MODULE_2__["LatestVideoService"]
+            ],
             styles: [__webpack_require__(/*! ./video-list.component.css */ "./src/app/video-list/video-list.component.css")]
         }),
-        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [_latest_video_service__WEBPACK_IMPORTED_MODULE_2__["LatestVideoService"]])
     ], VideoListComponent);
     return VideoListComponent;
 }());
@@ -234,6 +360,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
 /* harmony import */ var _videos_routing_module__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./videos-routing.module */ "./src/app/videos/videos-routing.module.ts");
 /* harmony import */ var _video_list_video_list_component__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../video-list/video-list.component */ "./src/app/video-list/video-list.component.ts");
+/* harmony import */ var _video_detail_video_detail_component__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../video-detail/video-detail.component */ "./src/app/video-detail/video-detail.component.ts");
+
 
 
 
@@ -244,7 +372,7 @@ var VideosModule = /** @class */ (function () {
     }
     VideosModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
         Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
-            declarations: [_video_list_video_list_component__WEBPACK_IMPORTED_MODULE_4__["VideoListComponent"]],
+            declarations: [_video_list_video_list_component__WEBPACK_IMPORTED_MODULE_4__["VideoListComponent"], _video_detail_video_detail_component__WEBPACK_IMPORTED_MODULE_5__["VideoDetailComponent"]],
             imports: [
                 _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
                 _videos_routing_module__WEBPACK_IMPORTED_MODULE_3__["VideosRoutingModule"]
